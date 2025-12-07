2396689a0f503f6b199a3c6ffa90983ff833282a
Ayad Masud <ayad.r.masud@gmail.com>
2025-12-06 18:29:27 -0600

    fixing all images

--- server/update_images.sql
+++ server/update_images.sql
@@ -1,11 +1,18 @@
-UPDATE menu_items 
-SET image_url = 'https://vintagekitchennotes.com/wp-content/uploads/2023/07/Tall-glasses-of-passionfruit-tea.jpeg' 
-WHERE name = 'Passionfruit Green Tea';
+-- Update all menu item images to match menu.ts
+UPDATE menu_items SET image_url = 'https://assets.epicurious.com/photos/5953ca064919e41593325d97/1:1/w_1920,c_limit/bubble_tea_recipe_062817.jpg' WHERE name = 'Original Milk Tea';
+UPDATE menu_items SET image_url = 'https://www.theflavorbender.com/wp-content/uploads/2020/06/Bubble-Milk-Tea-7511-1367x2048.jpg' WHERE name = 'Black Milk Tea';
+UPDATE menu_items SET image_url = 'https://images.squarespace-cdn.com/content/v1/62bf67c4d370c33d137d8f5c/b69b414d-3d6a-43a8-8a66-75df4e86d286/dong+ding+oolong+blog+banner+2.jpg' WHERE name = 'Oolong Milk Tea';
+UPDATE menu_items SET image_url = 'https://thaicaliente.com/wp-content/uploads/2024/08/thai-green-tea-feature.jpg' WHERE name = 'Green Milk Tea';
+UPDATE menu_items SET image_url = 'https://www.bubbletea.org/cdn/shop/products/cappuccino_BubbleTeaORG_1200x.jpg?v=1651725462' WHERE name = 'Capuccino Milk Tea';
+UPDATE menu_items SET image_url = 'https://www.souschef.co.uk/cdn/shop/articles/557539754042_f1b1ed7b-f9b8-4d9f-8fa4-0b2535788429_1080x.png?v=1761736933' WHERE name = 'Coconut Milk Tea';
+UPDATE menu_items SET image_url = 'https://maesmenu.com/wp-content/uploads/2023/06/ube-boba-9-500x500.jpg' WHERE name = 'Ube Milk Tea';
+UPDATE menu_items SET image_url = 'https://www.momontimeout.com/wp-content/uploads/2013/10/coffee-protein-shake.jpg' WHERE name = 'Protein Shake Milk Tea';
+UPDATE menu_items SET image_url = 'https://theicedcoffee.com/wp-content/uploads/2025/05/p93-3.webp' WHERE name = 'Ice Blend Latte';
+UPDATE menu_items SET image_url = 'https://delightfulplate.com/wp-content/uploads/2021/07/Winter-Melon-Tea-Tra-Bi-Dao.jpg' WHERE name = 'Winter Melon Green Tea';
+UPDATE menu_items SET image_url = 'https://vintagekitchennotes.com/wp-content/uploads/2023/07/Tall-glasses-of-passionfruit-tea.jpeg' WHERE name = 'Passionfruit Green Tea';
+UPDATE menu_items SET image_url = 'https://takestwoeggs.com/wp-content/uploads/2021/10/Mango-Green-Tea-Takestwoeggs-sq.jpg' WHERE name = 'Mango Green Tea';
+UPDATE menu_items SET image_url = 'https://images.unsplash.com/photo-1573500883698-e3ef47a95feb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzdHJhd2JlcnJ5JTIwZHJpbmslMjBsZW1vbmFkZXxlbnwxfHx8fDE3NjEzMTUzNDJ8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral' WHERE name = 'Strawberry Lemonade Tea';
+UPDATE menu_items SET image_url = 'https://www.acozykitchen.com/wp-content/uploads/2025/03/StrawberryMatchaLatte-1.jpg' WHERE name = 'Strawberry Matcha';
+UPDATE menu_items SET image_url = 'https://www.dessertfortwo.com/wp-content/uploads/2012/04/peach-iced-tea-3.jpg' WHERE name = 'Peach Oolong Tea';
+UPDATE menu_items SET image_url = 'https://images.unsplash.com/photo-1722478347147-7238a01b78d3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxtYXRjaGElMjBncmVlbiUyMHRlYXxlbnwxfHx8fDE3NjEyMzM1NzZ8MA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral' WHERE name = 'Secret Matcha';
 
-UPDATE menu_items 
-SET image_url = 'https://takestwoeggs.com/wp-content/uploads/2021/10/Mango-Green-Tea-Takestwoeggs-sq.jpg' 
-WHERE name = 'Mango Green Tea';
-
-UPDATE menu_items 
-SET image_url = 'https://www.dessertfortwo.com/wp-content/uploads/2012/04/peach-iced-tea-3.jpg' 
-WHERE name = 'Peach Oolong Tea';
